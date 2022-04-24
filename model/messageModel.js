cd59d905c857100e51344d2c515d13fc3d7b2b45
NHKhang132 <19521665@gm.uit.edu.vn>
2022-04-24 15:12:29 +0700

    messages

---
+++ model/messageModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const messagesSchema = new Schema ({
+    conversationId: {type: mongoose.Types.ObjectId, ref: 'convaersations'},
+    senderId: {type: mongoose.Types.ObjectId, ref: 'users'},
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+})
+
+module.exports = mongoose.model('messages', messagesSchema)
